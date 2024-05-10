abf5d0bceeafc749cc43b733273f629bcaa8e07f
K. Pramadu Shehan Chithakshana <151552571+Pramadu2001@users.noreply.github.com>
2024-05-10 13:11:53 +0530

    begin

---
+++ app/src/main/java/com/example/to_do_app/AddTaskActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.to_do_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTaskActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task2)
+    }
+}
